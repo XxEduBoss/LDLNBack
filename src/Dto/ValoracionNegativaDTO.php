c1b4f8720a8f20db55d91a9c31bd7f9064d24baf
juanramonbaezaborrego <jbaezaborrego@safareyes.es>
2024-01-17 19:45:46 +0100

    ValoracionNegativaDTO.php + ValoracionPositivaDTO.php

---
+++ src/Dto/ValoracionNegativaDTO.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Usuario;
+use App\Entity\Video;
+
+class ValoracionNegativaDTO
+{
+    private ?int $id = null;
+    private ?bool $dislikes = null;
+    private ?Video $video = null;
+    private ?Usuario $usuario = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getDislikes(): ?bool
+    {
+        return $this->dislikes;
+    }
+
+    /**
+     * @param bool|null $dislikes
+     */
+    public function setDislikes(?bool $dislikes): void
+    {
+        $this->dislikes = $dislikes;
+    }
+
+    /**
+     * @return Video|null
+     */
+    public function getVideo(): ?Video
+    {
+        return $this->video;
+    }
+
+    /**
+     * @param Video|null $video
+     */
+    public function setVideo(?Video $video): void
+    {
+        $this->video = $video;
+    }
+
+    /**
+     * @return Usuario|null
+     */
+    public function getUsuario(): ?Usuario
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param Usuario|null $usuario
+     */
+    public function setUsuario(?Usuario $usuario): void
+    {
+        $this->usuario = $usuario;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    /**
+     * @param bool|null $activo
+     */
+    public function setActivo(?bool $activo): void
+    {
+        $this->activo = $activo;
+    }
+    private ?bool $activo = null;
+
+
+
+
+
+
+}
---
+++ src/Dto/ValoracionPositivaDTO.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Usuario;
+use App\Entity\Video;
+
+class ValoracionPositivaDTO
+{
+    private ?int $id = null;
+    private ?bool $likes = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getLikes(): ?bool
+    {
+        return $this->likes;
+    }
+
+    /**
+     * @param bool|null $likes
+     */
+    public function setLikes(?bool $likes): void
+    {
+        $this->likes = $likes;
+    }
+
+    /**
+     * @return Video|null
+     */
+    public function getVideo(): ?Video
+    {
+        return $this->video;
+    }
+
+    /**
+     * @param Video|null $video
+     */
+    public function setVideo(?Video $video): void
+    {
+        $this->video = $video;
+    }
+
+    /**
+     * @return Usuario|null
+     */
+    public function getUsuario(): ?Usuario
+    {
+        return $this->usuario;
+    }
+
+    /**
+     * @param Usuario|null $usuario
+     */
+    public function setUsuario(?Usuario $usuario): void
+    {
+        $this->usuario = $usuario;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    /**
+     * @param bool|null $activo
+     */
+    public function setActivo(?bool $activo): void
+    {
+        $this->activo = $activo;
+    }
+    private ?Video $video = null;
+    private ?Usuario $usuario = null;
+    private ?bool $activo = null;
+
+
+
+}
