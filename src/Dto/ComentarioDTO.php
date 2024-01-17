73fd17409c458325256dc8d49e893905ca85a8c3
AntoCM22 <acamposmoreno@safareyes.es>
2024-01-17 19:43:25 +0100

    DTO COMENTARIO Y NOTIFICACION

---
+++ src/Dto/ComentarioDTO.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Usuario;
+use App\Entity\Video;
+
+class ComentarioDTO
+{
+
+    private ?int $id = null;
+    private ?string $texto = null;
+    private ?\DateTimeInterface $fecha_publicacion = null;
+    private ?Video $video = null;
+    private ?Usuario $usuario = null;
+    private ?bool $activo = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getTexto(): ?string
+    {
+        return $this->texto;
+    }
+
+    public function setTexto(?string $texto): void
+    {
+        $this->texto = $texto;
+    }
+
+    public function getFechaPublicacion(): ?\DateTimeInterface
+    {
+        return $this->fecha_publicacion;
+    }
+
+    public function setFechaPublicacion(?\DateTimeInterface $fecha_publicacion): void
+    {
+        $this->fecha_publicacion = $fecha_publicacion;
+    }
+
+    public function getVideo(): ?Video
+    {
+        return $this->video;
+    }
+
+    public function setVideo(?Video $video): void
+    {
+        $this->video = $video;
+    }
+
+    public function getUsuario(): ?Usuario
+    {
+        return $this->usuario;
+    }
+
+    public function setUsuario(?Usuario $usuario): void
+    {
+        $this->usuario = $usuario;
+    }
+
+    public function getActivo(): ?bool
+    {
+        return $this->activo;
+    }
+
+    public function setActivo(?bool $activo): void
+    {
+        $this->activo = $activo;
+    }
+
+}
